0639413720044be0b81dfad86d8c818648df4132
Robert Podgajny <robertpodgajny@gmail.com>
2018-03-16 13:04:17 +0100

    Exercise_3_part_1

--- django_twitter/twitter/models.py
+++ django_twitter/twitter/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Tweet(models.Model):
+    content = models.CharField(max_length=140)
+    creation_date = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
