3b9e53ca46d6875cdc80765a2fcb371d848e6ed9
Robert Podgajny <robertpodgajny@gmail.com>
2018-03-16 12:42:48 +0100

    Exercise_2

--- django_twitter/twitter/views.py
+++ django_twitter/twitter/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class MainPageView(View):
+    def get(self, request):
+        return render(request,
+                      template_name='base.html')
+
+
